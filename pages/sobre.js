9aa999c9f6fba460aa824d6abf26061bd7c7c464
Georges Silva <49493859+RedPoiint@users.noreply.github.com>
2021-09-19 16:55:17 -0300

    Create sobre.js

---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+function Sobre() {
+    return (<div>
+        <h1>Quem somos?</h1>
+
+        <Link href="/">
+            <a>Voltar a página principal</a>
+        </Link>
+    </div>)
+}
+
+export default Sobre
