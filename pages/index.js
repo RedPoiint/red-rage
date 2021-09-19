1be606106c85aef34cc8ce75426f73af5acfbb38
Georges Silva <49493859+RedPoiint@users.noreply.github.com>
2021-09-19 16:55:21 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,13 @@
-import { useState} from 'react';
+import Link from 'next/link';
 
 function Home() {
-    return (
-        <div>
-            <h1>RedRage</h1>
-            <Contador />
-        </div>
-    )
-}
+    return (<div>
+        <h1>RedRage</h1>
 
-function Contador() {
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
+        <Link href="/sobre">
+            <a>Acessar página Sobre</a>
+        </Link>
+    </div>)
 }
 
 export default Home
